fb2dca64359636bc7ad255b764629b8d0a2f58b1
Дмитрий Губенко <gornijinjener@gmail.com>
2023-09-06 15:57:47 +0300

    замечания ревьюера

--- homework.py
+++ homework.py
@@ -12,7 +12,7 @@ class InfoMessage:
     calories: float
     message: str = ('Тип тренировки: {training_type}; '
                     'Длительность: {duration:.3f} ч.; '
-                    'Дистанция: {distance:.3f} км ;'
+                    'Дистанция: {distance:.3f} км; '
                     'Ср. скорость: {speed:.3f} км/ч; '
                     'Потрачено ккал: {calories:.3f}.')
 
